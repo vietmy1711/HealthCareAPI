8589228d30b28906c33aa6bfd115432682d1a038
Lauv Edward <phuc.le@staff.gumiviet.com>
2021-05-18 14:52:35 +0700

    commit

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -1,27 +1,22 @@
 -- +migrate Up
-CREATE TABLE "account"
-(
-    "iduser"   varchar PRIMARY KEY,
-    "username" varchar,
-    "blood"    integer,
-    "gender"   integer
+CREATE TABLE "account" (
+                           "iduser" varchar PRIMARY KEY,
+                           "username" varchar,
+                           "blood" integer,
+                           "gender" integer
 );
 
-CREATE TABLE "healthday"
-(
-    "iduser"    varchar,
-    "createat"  date PRIMARY KEY,
-    "water"     integer,
-    "steps"     integer,
-    "heartrate" integer,
-    "calogries" integer,
-    "height"    float,
-    "weight"    float
+CREATE TABLE "healthday" (
+                             "iduser" varchar,
+                             "createat" date PRIMARY KEY,
+                             "water" integer,
+                             "steps" integer,
+                             "heartrate" integer,
+                             "calogries" integer,
+                             "height" float,
+                             "weight" float
 );
 
-ALTER TABLE "healthday"
-    ADD CONSTRAINT "acountheath" FOREIGN KEY ("iduser") REFERENCES "account" ("iduser");
+ALTER TABLE "healthday" ADD CONSTRAINT "acountheath" FOREIGN KEY ("iduser") REFERENCES "account" ("iduser");
 
 -- +migrate Down
-DROP TABLE "healthday";
-DROP TABLE "user";
