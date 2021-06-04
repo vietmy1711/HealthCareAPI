c1fb8b0cb675acbfb36a93df03b0ed8cf3df9616
Lauv Edward <phuc.le@staff.gumiviet.com>
2021-06-04 22:35:40 +0700

    get new value healthday

--- repository/repo_impl/healthday_impl.go
+++ repository/repo_impl/healthday_impl.go
@@ -77,7 +77,7 @@ func (u HealthDayRepoImpl) GetInforHealthInDay(context context.Context, userid s
 	fmt.Printf("get health day")
 	print(time.Now().String())
 	err := u.sql.DB.SelectContext(context, &listheathday, "SELECT userid, createat,  water, steps, heartrate, calories, height, weight, active_energy_bunred as active_energy_burned," +
-		"basal_energy_bunred as basal_energy_burned, blood_oxygen FROM healthday  WHERE userid = $1 LIMIT 1", userid )
+		"basal_energy_bunred as basal_energy_burned, blood_oxygen FROM healthday  WHERE userid = $1 ORDER BY createat::TIMESTAMP DESC LIMIT 1", userid )
 	if err != nil {
 		log.Error(err.Error())
 		return listheathday, err
